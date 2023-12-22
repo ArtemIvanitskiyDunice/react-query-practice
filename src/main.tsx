5f21c9ca10d4a6bc49c90d23b52ce8799bd708eb
Artem <nokerzekul@gmail.com>
2023-12-22 16:56:41 +0300

    feat: create queryClient with oprions and queryProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        //глобальные настройки для всего приложения, их много, в документации можно глянуть
+        queries: {
+            refetchOnWindowFocus: false,
+            //отключение повторного запроса по умолчанию при смене открытого окна
+        },
+    },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    </React.StrictMode>
 )
