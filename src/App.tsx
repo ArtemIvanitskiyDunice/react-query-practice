81bd4a4409bcb22a99e0f65bf799a8c56f4a9c0d
Artem <nokerzekul@gmail.com>
2023-12-25 18:11:05 +0300

    feat: made getAll from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
 import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
 import { ITodo } from './app.interface'
+import useTodoService from './services/todo.service';
+
 
 const todoId = 1
 
 function App() {
-    const { isLoading, error, data } = useQuery({
-        queryKey: ['todos', todoId],
+    const {getAll, getById}  = useTodoService();
+
+    const { isLoading, isError, error, data } = useQuery({
+        queryKey: ['todos'],
         //ключ для взаимодействия react-query
-        queryFn: () =>
-            axios.get<ITodo>('https://jsonplaceholder.typicode.com/todos/1'),
+        queryFn: () => getAll(),
         select: ({ data }) => data,
         //select позволяет после успешного получения данных их трансформировать на стороне react-query
     })
+
+    if (isError) {
+        return <span>Error: {error.message}</span>
+    }
+
+    function renderItems (arr: ITodo[]) {
+        const items = arr.map((item) => {
+            return (
+            <li key={item.id}><b>{item.id}</b> {item.title}</li>
+            )  
+        })
+        return (
+            <ul>
+                {items}
+            </ul>
+        )
+    }
+
+    const todos: JSX.Element | null = data ? renderItems(data) : null;
 
     return (
         <div>
             {isLoading ? (
                 <div> Loading...</div>
             ) : data ? (
-                <h1>Todo: {data.title}</h1>
+                <h1>Todos: {todos}</h1>
             ) : (
                 <h1>Data not found!</h1>
             )}
