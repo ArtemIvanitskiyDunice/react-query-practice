73605c9c97ca6e71930fe7b31ab4a495f1bba28d
Artem <nokerzekul@gmail.com>
2023-12-25 18:10:32 +0300

    feat: made service

---
+++ src/services/todo.service.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { ITodo } from "../app.interface";
+
+const useTodoService = () => {
+    const URL = 'https://jsonplaceholder.typicode.com/todos';
+    
+    const getById = async (id: string) => {
+        return axios.get<ITodo>(`${URL}/${id}`);
+    }
+    const getAll = async () => {
+        return axios.get<ITodo[]>(`${URL}`);
+    }
+    return {
+        getById,
+        getAll,
+    }
+}
+
+export default useTodoService
