3020a1f99de36020f2da2d19cc6bf32dca246435
Artem <nokerzekul@gmail.com>
2023-12-25 16:34:54 +0300

    feat: add select and conditions for display on the site, depending on the loading status

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import { useQuery } from "@tanstack/react-query"
-import axios from "axios";
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import { ITodo } from './app.interface'
 
-const todoId = 1;
+const todoId = 1
 
 function App() {
-
-	const {isLoading, error, data} = useQuery({
-        queryKey:['todos', todoId],
-        queryFn:() => 
-            axios.get('https://jsonplaceholder.typicode.com/todos/1'),
-    });
+    const { isLoading, error, data } = useQuery({
+        queryKey: ['todos', todoId],
+        //ключ для взаимодействия react-query
+        queryFn: () =>
+            axios.get<ITodo>('https://jsonplaceholder.typicode.com/todos/1'),
+        select: ({ data }) => data,
+        //select позволяет после успешного получения данных их трансформировать на стороне react-query
+    })
 
     return (
         <div>
-            {data ? <h1>Todo: {data.data.title}</h1> : <h1>Data not found!</h1>}
+            {isLoading ? (
+                <div> Loading...</div>
+            ) : data ? (
+                <h1>Todo: {data.title}</h1>
+            ) : (
+                <h1>Data not found!</h1>
+            )}
         </div>
     )
 }
