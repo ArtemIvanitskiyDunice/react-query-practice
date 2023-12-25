180288043c4a97d210f14953cea18b547121d833
Artem <nokerzekul@gmail.com>
2023-12-25 19:25:26 +0300

    feat: useTodo hook

---
+++ src/hooks/useTodo.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query"
+import useTodoService from "../services/todo.service"
+
+export const useTodo = (todoId) => {
+    const { getById } = useTodoService()
+    return useQuery({
+        queryKey: ['todo', todoId],
+        //ключ для взаимодействия react-query, для каждой операции должен быть свой
+        queryFn: () => getById(todoId),
+        select: ({ data }) => data,
+        //select позволяет после успешного получения данных их трансформировать на стороне react-query
+        retry: 10,
+        //количество повторных запросов до получения ошибки
+        enabled:!!todoId,
+        //запрос будет работать только при выполнении условий, в данном случае обязательно наличие айди в ключе
+    })
+}
